81ea6adb51e9b6e97e056771fc45f0d20efd8603
Muntaka Helali <helali.muntaka@gmail.com>
2022-05-12 04:49:58 -0400

    Implemented the Database class and created a database property that returns a ForageDatabse instance using lazy initialization in the BaseApplication.kt .

--- app/src/main/java/com/example/forage/data/ForageDatabase.kt
+++ app/src/main/java/com/example/forage/data/ForageDatabase.kt
@@ -15,6 +15,9 @@
  */
 package com.example.forage.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.forage.model.Forageable
 
@@ -23,4 +26,29 @@ import com.example.forage.model.Forageable
  * This database stores a [Forageable] entity
  */
 // TODO: create the database with all necessary annotations, methods, variables, etc.
-abstract class ForageDatabase : RoomDatabase()
+@Database(entities = [Forageable::class], version=1, exportSchema = false)
+abstract class ForageDatabase: RoomDatabase() {
+    abstract fun forageableDao(): ForageableDao
+
+    companion object {
+        /*The value of a volatile variable will never be cached, and all writes and reads will be done to and from the main memory.
+        This helps make sure the value of INSTANCE is always up-to-date and the same for all execution threads.
+        It means that changes made by one thread to INSTANCE are visible to all other threads immediately.
+        */
+        @Volatile
+        private var INSTANCE: ForageDatabase? = null
+        fun getDatabase(context: Context) : ForageDatabase{
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ForageDatabase::class.java,
+                    "forageable_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
